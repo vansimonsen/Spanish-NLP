efe99c54b69891541c7993f65a33545b7cbb43ea
vansimonsen <vansimonsen@gmail.com>
2016-09-20 17:54:28 -0300

    running SVM on reforms datasets

--- SVM.py
+++ SVM.py
@@ -20,13 +20,7 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import cross_val_score
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer      
-
-
-#Required packages from nltk
-#nltk.download('punkt')
-#nltk.download('stopwords')
-
+from sklearn.feature_extraction.text import CountVectorizer    
 
 
 #Stem: Cut word in root (wait: wait, waited: wait, waiting: wait)
@@ -50,9 +44,6 @@ def tokenize(text):
         stems = ['']
     return stems
 
-
-
-
 try:
     general_tweets_corpus_train = pd.read_csv('datasets/csv/general-tweets-train-tagged.csv', encoding='utf-8')
 except:
@@ -72,60 +63,85 @@ except:
     general_tweets_corpus_train.to_csv('datasets/csv/general-tweets-train-tagged.csv', index=False, encoding='utf-8')
 
 
-tweets_corpus = pd.concat([general_tweets_corpus_train])
-tweets_corpus = tweets_corpus.query('agreement != "DISAGREEMENT" and polarity != "NONE"')
-tweets_corpus = tweets_corpus[-tweets_corpus.content.str.contains('^http.*$')]
-
-
 
 try:
-    general_tweets_corpus_test = pd.read_csv('datasets/csv/general-tweets-test1k.csv')#, encoding='utf-8')
+    re_test = pd.read_csv('reformas/csv/RE.csv', encoding='utf-8')
 except:
-    xml = objectify.parse(open('datasets/xml/general-tweets-test1k.xml'))
+    xml = objectify.parse(open('reformas/xml/RE.xml'))
     #sample tweet object
     root = xml.getroot()
-    general_tweets_corpus_test = pd.DataFrame(columns=('content', 'polarity'))
+    re_test = pd.DataFrame(columns=('content', 'polarity'))
     tweets = root.getchildren()
     for i in range(0,len(tweets)):
         tweet = tweets[i]
         row = dict(zip(['content'], [tweet.content.text]))
         row_s = pd.Series(row)
-        row_s.name = i
-        general_tweets_corpus_test = general_tweets_corpus_test.append(row_s)
-    general_tweets_corpus_test.to_csv('datasets/csv/general-tweets-test1k.csv', index=False)#, encoding='utf-8')
-
-
-tweets_test = pd.concat([general_tweets_corpus_test])
-
-
+        if row_s[0] != None:
+            row_s.name = i
+            re_test = re_test.append(row_s)
+    re_test.to_csv('reformas/csv/RE.csv', index=False, encoding='utf-8')
 
 
 try:
-    tagged_tweets_corpus_test = pd.read_csv('datasets/csv/general-tweets-test1k-tagged.csv', encoding='utf-8')
+    rc_test = pd.read_csv('reformas/csv/RC.csv', encoding='utf-8')
 except:
-
-    from lxml import objectify
-    xml = objectify.parse(open('datasets/xml/general-tweets-test1k-tagged.xml'))
+    xml = objectify.parse(open('reformas/xml/RC.xml'))
     #sample tweet object
     root = xml.getroot()
-    tagged_tweets_corpus_test = pd.DataFrame(columns=('content', 'polarity'))
+    rc_test = pd.DataFrame(columns=('content', 'polarity'))
     tweets = root.getchildren()
     for i in range(0,len(tweets)):
         tweet = tweets[i]
-        row = dict(zip(['content', 'polarity', 'agreement'], [tweet.content.text, tweet.sentiments.polarity.value.text]))
+        row = dict(zip(['content'], [tweet.content.text]))
         row_s = pd.Series(row)
-        row_s.name = i
-        tagged_tweets_corpus_test = tagged_tweets_corpus_test.append(row_s)
-    tagged_tweets_corpus_test.to_csv('datasets/csv/general-tweets-test1k-tagged.csv', index=False, encoding='utf-8')
+        if row_s[0] != None:
+            row_s.name = i
+            rc_test = rc_test.append(row_s)
+    rc_test.to_csv('reformas/csv/RC.csv', index=False, encoding='utf-8')
+
+try:
+    rt_test = pd.read_csv('reformas/csv/RT.csv', encoding='utf-8')
+except:
+    xml = objectify.parse(open('reformas/xml/RT.xml'))
+    #sample tweet object
+    root = xml.getroot()
+    rt_test = pd.DataFrame(columns=('content', 'polarity'))
+    tweets = root.getchildren()
+    for i in range(0,len(tweets)):
+        tweet = tweets[i]
+        row = dict(zip(['content'], [tweet.content.text]))
+        row_s = pd.Series(row)
+        if row_s[0] != None:
+            row_s.name = i
+            rt_test = rt_test.append(row_s)
+    rt_test.to_csv('reformas/csv/RT.csv', index=False, encoding='utf-8')
+
+try:
+    rl_test = pd.read_csv('reformas/csv/RL.csv', encoding='utf-8')
+except:
+    xml = objectify.parse(open('reformas/xml/RL.xml'))
+    #sample tweet object
+    root = xml.getroot()
+    rl_test = pd.DataFrame(columns=('content', 'polarity'))
+    tweets = root.getchildren()
+    for i in range(0,len(tweets)):
+        tweet = tweets[i]
+        row = dict(zip(['content'], [tweet.content.text]))
+        row_s = pd.Series(row)
+        if row_s[0] != None:
+            row_s.name = i
+            rl_test = rl_test.append(row_s)
+    rl_test.to_csv('reformas/csv/RL.csv', index=False, encoding='utf-8')
 
 
+re_test = pd.concat([re_test])
+rc_test = pd.concat([rc_test])
+rl_test = pd.concat([rl_test])
+rt_test = pd.concat([rt_test])
 
-tweets_tagged = pd.concat([tagged_tweets_corpus_test])
-tweets_tagged = tweets_tagged.query('polarity != "NONE"')
-diff = np.setdiff1d(tweets_test.index.values, tweets_tagged.index.values)
-
-tweets_test = tweets_test.drop(diff)
-
+tweets_corpus = pd.concat([general_tweets_corpus_train])
+tweets_corpus = tweets_corpus.query('agreement != "DISAGREEMENT" and polarity != "NONE"')
+tweets_corpus = tweets_corpus[-tweets_corpus.content.str.contains('^http.*$')]
 
 #Stopwords: Empty word (i.e articles)
 
@@ -139,14 +155,12 @@ non_words.extend(['¿', '¡'])
 non_words.extend(map(str,range(10)))
 
 
-
-
 #Binarizing
 
 tweets_corpus['polarity_bin'] = 0
 index = tweets_corpus.polarity.isin(['P', 'P+'])
 tweets_corpus.polarity_bin.loc[index] = 1
-print tweets_corpus.polarity_bin.value_counts(normalize=True)
+
 
 tweets_test['polarity_bin'] = 0
 
@@ -157,71 +171,42 @@ tweets_tagged.polarity_bin.value_counts(normalize=True)
 
 y = tweets_tagged.polarity_bin.values
 
+### BEST PARAMS
 
-vectorizer = CountVectorizer(
-                analyzer = 'word',
-                tokenizer = tokenize,
-                lowercase = True,
-                stop_words = spanish_stopwords)
+best_params = {'vect__ngram_range': (1, 2), 'cls__loss': 'hinge', 'vect__max_df': 0.5
+ , 'cls__max_iter': 1000, 'vect__min_df': 10, 'vect__max_features': 1000
+ , 'cls__C': 0.2}
 
-pipeline = Pipeline([
-    ('vect', vectorizer),
-    ('cls', LinearSVC()),
+best_pipe = Pipeline([
+    ('vect', CountVectorizer(
+            analyzer = 'word',
+            tokenizer = tokenize,
+            lowercase = True,
+            stop_words = spanish_stopwords,
+            min_df = 10,
+            max_df = 0.5,
+            ngram_range=(1, 2),
+            max_features=1000
+            )),
+    ('cls', LinearSVC(C=.2, loss='hinge',max_iter=1000,multi_class='ovr',
+             random_state=None,
+             penalty='l2',
+             tol=0.0001
+             )),
 ])
 
+best_pipe.fit(tweets_corpus.content, tweets_corpus.polarity_bin)
 
 
-parameters = {
-    'vect__max_df': (0.5, 1.9),
-    'vect__min_df': (10, 20,50),
-    'vect__max_features': (500, 1000),
-    'vect__ngram_range': ((1, 1), (1, 2)),  # unigrams or bigrams
-    'cls__C': (0.2, 0.5, 0.7),
-    'cls__loss': ('hinge', 'squared_hinge'),
-    'cls__max_iter': (500, 1000)
-}
+re_test['polarity'] = best_pipe.predict(re_test.content)
+rc_test['polarity'] = best_pipe.predict(rc_test.content)
+rl_test['polarity'] = best_pipe.predict(rl_test.content)
+rt_test['polarity'] = best_pipe.predict(rt_test.content)
 
-
-grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1 , scoring='roc_auc')
-grid_search.fit(tweets_corpus.content, tweets_corpus.polarity_bin)
-
-
-print grid_search.best_params_
-
-from sklearn.externals import joblib
-joblib.dump(grid_search, 'grid_search.pkl')
-
-
-model = LinearSVC(C=.2, loss='squared_hinge',max_iter=1000,multi_class='ovr',
-              random_state=None,
-              penalty='l2',
-              tol=0.0001
-)
-
-vectorizer = CountVectorizer(
-    analyzer = 'word',
-    tokenizer = tokenize,
-    lowercase = True,
-    stop_words = spanish_stopwords,
-    min_df = 50,
-    max_df = 1.9,
-    ngram_range=(1, 1),
-    max_features=1000
-)
-
-corpus_data_features = vectorizer.fit_transform(tweets_corpus.content)
-corpus_data_features_nd = corpus_data_features.toarray()
-
-
-scores = cross_val_score(
-    model,
-    corpus_data_features_nd[0:len(tweets_corpus)],
-    y=tweets_corpus.polarity_bin,
-    scoring='roc_auc',
-    cv=5
-    )
-
-print scores.mean()
+re_test.to_csv('re_predicted.csv')
+rc_test.to_csv('rc_predicted.csv')
+rl_test.to_csv('rl_predicted.csv')
+rt_test.to_csv('rt_predicted.csv')
 
 
 
